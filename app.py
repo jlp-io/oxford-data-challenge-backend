401cc4521b8d5e79154cbaa15db5da7e3b1f9f5b
jlp-io <jlp3@illinois.edu>
2019-04-27 23:43:34 +0100

    heroku

--- app.py
+++ app.py
@@ -91,4 +91,5 @@ if not app.debug:
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)    
+    app.run(port=port)    
+    #host='0.0.0.0'
